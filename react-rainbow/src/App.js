23cd8de9891b73367a7a5d108633419c8d50518b
Michael Champlin <mchamplin@sbcglobal.net>
2022-04-17 14:31:48 -0700

    added Rainbow React project

---
+++ react-rainbow/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
+
+function App(){
+    let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock key={i} color={color}/>
+        )
+    })
+    const addColor = (newColor) => {
+      setColors([...colors, newColor])
+  }
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm addColor={addColor}></ColorForm>
+        </div>
+    )
+}
+
+export default App;
